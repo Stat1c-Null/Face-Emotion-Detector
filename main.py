7df006794e2710e4dd464084567e439cea84e41f
Mikita Slabysh <slabysh2015@gmail.com>
2024-06-17 12:53:01 -0400

    Improved styling

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ face_detect_model = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 #Get webcam access
 camVideo = cv2.VideoCapture(0)
 
+squareColor = (180, 40, 160)
+
 while True:
   _, img = camVideo.read()
   grayFilter = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Put gray filter over video
@@ -14,7 +16,7 @@ while True:
   faces = face_detect_model.detectMultiScale(grayFilter, 1.1, 5)
   #For every face found, put square around it
   for (x, y, w, h) in faces:
-    cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.rectangle(img, (x, y), (x+w, y+h), squareColor, 2)
   cv2.imshow('img', img)
 
   #Press ESCAPE to close everything
